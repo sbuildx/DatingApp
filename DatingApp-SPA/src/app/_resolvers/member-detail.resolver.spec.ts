5fac22a9f3b2256ee56351bd98171b1f5938c364
Iskander <isa-cool@list.ru>
2020-09-07 19:59:45 +0600

    End of module 9

---
+++ DatingApp-SPA/src/app/_resolvers/member-detail.resolver.spec.ts
@@ -0,0 +1,7 @@
+import { MemberDetail.Resolver } from './member-detail.resolver';
+
+describe('MemberDetail.Resolver', () => {
+  it('should create an instance', () => {
+    expect(new MemberDetail.Resolver()).toBeTruthy();
+  });
+});
---
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.spec.ts
@@ -0,0 +1,7 @@
+import { MemberList.Resolver } from './member-list.resolver';
+
+describe('MemberList.Resolver', () => {
+  it('should create an instance', () => {
+    expect(new MemberList.Resolver()).toBeTruthy();
+  });
+});
